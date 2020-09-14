6cb692bc31fee51586ae0c05b2b05a6a1e114a04
tatsuya828828 <kkjy011894@gmail.com>
2020-09-14 11:28:07 +0900

    SecurityGetUserプロジェクト作成

---
+++ src/main/java/com/kuma/SecurityGetUserApplication.java
@@ -0,0 +1,13 @@
+package com.kuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGetUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityGetUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuma/SecurityGetUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGetUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
