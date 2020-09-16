77ac8476a09a624f9f0d9085de6faf79162be1ea
tatsuya828828 <kkjy011894@gmail.com>
2020-09-16 15:00:05 +0900

    UserDetailsを用いた場合の処理に変更

--- src/main/java/com/kuma/controller/HomeController.java
+++ src/main/java/com/kuma/controller/HomeController.java
@@ -5,10 +5,11 @@ import java.security.Principal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.kuma.domain.model.AppUserDetails;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,7 +21,7 @@ public class HomeController {
 	// @AuthenticationPrincipalをUserクラスにつけると、Userクラスにログイン情報が渡される
 	// このUserクラスは、Springが用意しているクラスである
 	@GetMapping("/home")
-	public String getHome(Model model, @AuthenticationPrincipal User user) {
+	public String getHome(Model model, @AuthenticationPrincipal AppUserDetails user) {
 		log.info("HomeController Start");
 
 		// ログインユーザー情報の表示
@@ -37,12 +38,12 @@ public class HomeController {
 	public String getHome2(Model model, Principal principal) {
 		// ログインユーザー情報の取得
 		Authentication authentication = (Authentication) principal;
-		User user1 = (User) authentication.getPrincipal();
+		AppUserDetails user1 = (AppUserDetails) authentication.getPrincipal();
 		log.info("user1:"+ user1.toString());
 
 		// ログインユーザー情報の取得
 		// SecurityContextHolderを使うことでも、ユーザー情報を取得することができる
-		User user2 = (User) SecurityContextHolder
+		AppUserDetails user2 = (AppUserDetails) SecurityContextHolder
 				.getContext().getAuthentication().getPrincipal();
 		log.info("user2:"+ user2.toString());
 		return "home";
